76513d13b991ea6a10fdeb98e2ba48adc197c891
Mark Harviston <mark.harviston@gmail.com>
2015-01-12 23:34:47 -0800

    wait&passthru

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -36,11 +36,11 @@ function main () {
     }
     if ($env:QTIMPL -eq "PyQt4"){
         #Invoke-WebRequest "http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py3.4-Qt4.8.6-x32.exe" -OutFile C:\install-PyQt4.exe -MaximumRedirection 10
-        ((new-object net.webclient).DownloadFile("http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py3.4-Qt4.8.6-x32.exe", "C:\install-PyQt4.exe"))
+        (new-object net.webclient).DownloadFile("http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py3.4-Qt4.8.6-x32.exe", "C:\install-PyQt4.exe")
         Write-Host "##DEBUG##"
-        dir
+        dir C:\
         Write-Host "##/DEBUG##"
-        Start-Process -FilePath C:\install-PyQt4.exe -ArgumentList "/S"
+        Start-Process -FilePath C:\install-PyQt4.exe -ArgumentList "/S" -Wait -Passthru
     }
 }
 
