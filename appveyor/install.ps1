9e6190b5cfe9375a906f6e45cbdd01551dea1e4e
Mark Harviston <mark.harviston@gmail.com>
2015-01-12 23:22:03 -0800

    use net.webclient instead of invoke-webrequest

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -35,9 +35,10 @@ function main () {
         InstallPackage $env:Python PySide
     }
     if ($env:QTIMPL -eq "PyQt4"){
-        Invoke-WebRequest "http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py3.4-Qt4.8.6-x32.exe" -OutFile C:\install-PyQt4.exe -MaximumRedirection 10
+        #Invoke-WebRequest "http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py3.4-Qt4.8.6-x32.exe" -OutFile C:\install-PyQt4.exe -MaximumRedirection 10
+        ((new-object net.webclient).DownloadFile("http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py3.4-Qt4.8.6-x32.exe", "C:\install-PyQt4.exe"))
         Write-Host "##DEBUG##"
-        dir C:\
+        dir
         Write-Host "##/DEBUG##"
         Start-Process -FilePath C:\install-PyQt4.exe -ArgumentList "/S"
     }
